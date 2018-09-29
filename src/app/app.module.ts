4c8a4ac1a5b555ef9a0ae4d3c0d8803eaacd504f
Mikoo Saguindang <mikoo@getmercuryapp.com>
2018-09-29 16:07:15 +0800

    added: signin component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SigninComponent } from './components/pages';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        SigninComponent
+    ],
+    imports: [
+        BrowserModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/components/pages/signin/signin.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component ({
+    selector: 'app-signin',
+    templateUrl: './signin.component.html'
+})
+export class SigninComponent implements OnInit {
+
+    constructor() {
+        this.ngOnInit();
+    }
+
+    ngOnInit() {
+        console.log('initialized signin component');
+    }
+}
+
